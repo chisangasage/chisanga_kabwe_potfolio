22bdb03e83ac9a69a0640d2fa441a5397117107c
chisangasage <chisangasage@gmail.com>
2025-10-23 23:20:45 +0200

    more hosts

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-4q6@ykwc6eq8_vwip&k0p6j3*dgvd4p@3%pb39g-hyasr=81!j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['chisanga-kabwe-potfolio.onrender.com']
+ALLOWED_HOSTS = [
+        'chisanga-kabwe-potfolio.onrender.com',
+        '127.0.0.1',
+        'localhost',
+    ]
 
 
 # Application definition
